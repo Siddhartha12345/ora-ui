7e39e450f4a658211968c0d36cc46e898dfe62c4
Siddhartha12345 <siddharthapaul101@gmail.com>
2024-09-03 21:25:53 +0530

    designed about us screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 const routes: Routes = [
-  { path: '', component: LandingPageComponent }
+  { path: '', component: LandingPageComponent },
+  { path: 'about', component: AboutusComponent }
 ];
 
 @NgModule({
