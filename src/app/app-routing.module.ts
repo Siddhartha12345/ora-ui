b0a69f6d2ae9921c8f176680c714b2d32d39f841
Siddhartha12345 <siddharthapaul101@gmail.com>
2024-09-03 21:47:42 +0530

    designed contact us page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
-  { path: 'about', component: AboutusComponent }
+  { path: 'about', component: AboutusComponent },
+  { path: 'contact', component: ContactusComponent }
 ];
 
 @NgModule({
