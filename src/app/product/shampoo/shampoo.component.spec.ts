a80b9570cb2a5db68e34f5cf5647709c3211ceae
Siddhartha12345 <siddharthapaul101@gmail.com>
2024-09-07 13:48:59 +0530

    fixed css issue

---
+++ src/app/product/shampoo/shampoo.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShampooComponent } from './shampoo.component';
+
+describe('ShampooComponent', () => {
+  let component: ShampooComponent;
+  let fixture: ComponentFixture<ShampooComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ShampooComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ShampooComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
