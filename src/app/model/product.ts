fb085f084279d5fd699a3ac77febf5260aa244ee
Siddhartha12345 <siddharthapaul101@gmail.com>
2024-09-22 02:06:41 +0530

    mock shampoo products

---
+++ src/app/model/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+    productId: string,
+    productName: string,
+    productImage: string,
+    productAvgRating: number,
+    productTotalRating: number,
+    productPrice: number
+}
