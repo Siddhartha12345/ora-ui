c5b2124eb5477798b75619e233b29719b5513a2b
Siddhartha12345 <siddharthapaul101@gmail.com>
2024-09-22 23:00:03 +0530

    hair serum list screen

--- src/app/products/hair-serums/hair-serums.component.ts
+++ src/app/products/hair-serums/hair-serums.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { hairSerumMockList } from '../../../assets/mock/hair-serum-mock';
+import { Product } from '../../model/product';
+import { ProductUtil } from '../../util/product-util';
+import { CommonModule } from '@angular/common';
+import { StarRatingComponent } from '../star-rating/star-rating.component';
 
 @Component({
   selector: 'app-hair-serums',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, StarRatingComponent],
   templateUrl: './hair-serums.component.html',
-  styleUrl: './hair-serums.component.css'
+  styleUrls: ['./hair-serums.component.css', '../products-styles.css']
 })
 export class HairSerumsComponent {
 
+  hairSerumList: Product[] = hairSerumMockList;
+
+  // Method to toggle bookmark state
+  toggleBookmark(productId: string): void {
+    ProductUtil.toggleBookmark(this.hairSerumList, productId);
+  }
+
+  addToCart(productId: string): void {
+    ProductUtil.addToCart(this.hairSerumList, productId);
+  }
 }
