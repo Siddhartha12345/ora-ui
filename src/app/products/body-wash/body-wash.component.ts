85149d569c1d0b419ccfe06ce2e12fda7dd9cf6b
Siddhartha12345 <siddharthapaul101@gmail.com>
2024-09-23 00:05:08 +0530

    body wash product list

--- src/app/products/body-wash/body-wash.component.ts
+++ src/app/products/body-wash/body-wash.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { StarRatingComponent } from '../star-rating/star-rating.component';
+import { bodyWashMockList } from '../../../assets/mock/body-wash-mock';
+import { Product } from '../../model/product';
+import { ProductUtil } from '../../util/product-util';
 
 @Component({
   selector: 'app-body-wash',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, StarRatingComponent],
   templateUrl: './body-wash.component.html',
-  styleUrl: './body-wash.component.css'
+  styleUrls: ['./body-wash.component.css', '../products-styles.css']
 })
 export class BodyWashComponent {
 
+  bodyWashList: Product[] = bodyWashMockList;
+
+  // Method to toggle bookmark state
+  toggleBookmark(productId: string): void {
+    ProductUtil.toggleBookmark(this.bodyWashList, productId);
+  }
+
+  addToCart(productId: string): void {
+    ProductUtil.addToCart(this.bodyWashList, productId);
+  }
 }
