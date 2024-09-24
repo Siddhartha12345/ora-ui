7a19bfd4268effb6516539d050fd260de9132a50
Siddhartha12345 <siddharthapaul101@gmail.com>
2024-09-25 01:12:08 +0530

    product view impl

--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -4,9 +4,12 @@ export interface Product {
     productId: string,
     productName: string,
     productImage: string,
+    shortDescription: string,
     productAvgRating: number,
     productTotalRating: number,
     productPrice: number,
+    productQty: number,
+    productOldPrice: number,
     isBookmarked: boolean,   // this field will not be a property of products db but will be coming from user cart db 
     isAddedToCart: boolean,  // this field will not be a property of products db but will be coming from user cart db
     productType: ProductType
