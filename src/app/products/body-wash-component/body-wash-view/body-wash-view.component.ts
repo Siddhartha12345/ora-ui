89da01569f8e6ae36392e9e78944677bab8b7838
Siddhartha12345 <siddharthapaul101@gmail.com>
2024-09-24 21:06:57 +0530

    product view design

--- src/app/products/body-wash-component/body-wash-view/body-wash-view.component.ts
+++ src/app/products/body-wash-component/body-wash-view/body-wash-view.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-body-wash-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './body-wash-view.component.html',
   styleUrl: './body-wash-view.component.css'
 })
